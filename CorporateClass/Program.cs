7290a55336c84f448cd1f92c194dff12200e222d
Ripal Patel <ripal212007@gmail.com>
2019-03-05 13:45:06 -0600

    Add project files.

---
+++ CorporateClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CorporateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
